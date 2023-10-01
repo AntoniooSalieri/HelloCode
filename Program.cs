6c919dd4c5249dce8215ae766dc8f904ef11f593
IvanPavlovich <antonio.fon.salieri@gmail.com>
2023-10-01 23:33:50 +0300

    some strange files added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string username = Console.ReadLine();
+
+if (username.ToLower == "полина")
+{
+    Console.WrightLine("Привет, гриб-кринжик! <3")
+
+}
+else
+{
+    Console.WrightLine("Не привет, не гриб-кринжик. :<")
+}
