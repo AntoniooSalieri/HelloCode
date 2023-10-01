a7e4bd84f347cece54a1204b8edb6eb518c867f9
IvanPavlovich <antonio.fon.salieri@gmail.com>
2023-10-01 23:19:30 +0300

    example 4 done

---
+++ example005_random/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(-10,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(-10,10);
+Console.WriteLine(numberB);
+
+Console.WriteLine(numberA+numberB);
